1b19fb4b6f01ffe464b15e2acb039528926b1f3b
Rafael Bicalho <rbmbika@gmail.com>
2019-11-09 18:15:29 -0300

    Remove Redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,8 +8,8 @@ const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(sagaMiddleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 sagaMiddleware.run(rootSaga);
