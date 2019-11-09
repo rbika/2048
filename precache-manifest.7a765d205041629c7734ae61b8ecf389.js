85b74f09fbfd7c4b66b473c8d26b9c6860b430d4
Rafael Bicalho <rbmbika@gmail.com>
2019-11-09 17:37:42 -0300

    Updates

---
+++ precache-manifest.7a765d205041629c7734ae61b8ecf389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5577c8a80288f119c90325b8fa8bc5",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "64b47930d43876375045",
+    "url": "/2048/static/css/main.052f2973.chunk.css"
+  },
+  {
+    "revision": "48f77ded49be60e12313",
+    "url": "/2048/static/js/2.a3447f89.chunk.js"
+  },
+  {
+    "revision": "64b47930d43876375045",
+    "url": "/2048/static/js/main.74a74491.chunk.js"
+  },
+  {
+    "revision": "ac8e2380c5f68ee5b0c2",
+    "url": "/2048/static/js/runtime-main.effbb987.js"
+  }
+]);
